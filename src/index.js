cee49569e595a7567e3059745a833c600eec4dda
guillaumeAder1 <GAder@DUBLT01926.esri-holdings.com>
2017-07-27 15:24:30 +0100

    add reducer as external file

--- src/index.js
+++ src/index.js
@@ -1,18 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore } from 'redux';
+import userReducer from './userReducer.js'
 import './index.css';
 
-const userReducer = function (state, action) {
-	if (action.type === 'INC') {
-		const newState = Object.assign({}, state, { user: { age: state.user.age + action.payload } })
-		return newState;
-	} else if (action.type === 'DEC') {
-		const newState = Object.assign({}, state, { user: { age: state.user.age - action.payload } })
-		return newState;
-	}
-	return state;
-}
+// const userReducer = function (state, action) {
+// 	if (action.type === 'INC') {
+// 		const newState = Object.assign({}, state, { user: { age: state.user.age + action.payload } })
+// 		return newState;
+// 	} else if (action.type === 'DEC') {
+// 		const newState = Object.assign({}, state, { user: { age: state.user.age - action.payload } })
+// 		return newState;
+// 	}
+// 	return state;
+// }
+
 
 const store = createStore(userReducer, {
 	user: {
---
+++ src/userReducer.js
@@ -0,0 +1,13 @@
+
+const userReducer = (state, action) => {
+	if (action.type === 'INC') {
+		const newState = Object.assign({}, state, { user: { age: state.user.age + action.payload } })
+		return newState;
+	} else if (action.type === 'DEC') {
+		const newState = Object.assign({}, state, { user: { age: state.user.age - action.payload } })
+		return newState;
+	}
+	return state;
+}
+
+export default userReducer;
